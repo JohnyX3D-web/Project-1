d50248dde6b124f5144332b3777576aac054bba4
Jonyx3d <dinisan2112@gmail.com>
2022-07-09 22:10:48 +0300

    close menu on escape

--- js/main.js
+++ js/main.js
@@ -2,15 +2,36 @@ const burgerBtn = document.querySelector(".header__burger-btn");
 const navList = document.querySelector(".header__nav-list");
 const linkNav = document.querySelectorAll(".header__nav-item a");
 
+function closeMenuOnEscape(event) {
+	console.log(event.code);
+	if (event.code === "Escape") {
+		closeMenu();
+	}
+}
+
 burgerBtn.addEventListener("click", function () {
-	burgerBtn.classList.toggle("header__burger-btn--open");
-	navList.classList.toggle("header__nav-list--active");
+	if (burgerBtn.classList.contains("header__burger-btn--open")) {
+		closeMenu();
+	} else {
+		openMenu();
+	}
 });
+
+function openMenu() {
+	document.addEventListener("keydown", closeMenuOnEscape);
+	burgerBtn.classList.add("header__burger-btn--open");
+	navList.classList.add("header__nav-list--active");
+}
+
+function closeMenu() {
+	document.removeEventListener("keydown", closeMenuOnEscape);
+	burgerBtn.classList.remove("header__burger-btn--open");
+	navList.classList.remove("header__nav-list--active");
+}
 
 linkNav.forEach((link) => {
 	link.addEventListener("click", function (event) {
-		burgerBtn.classList.remove("header__burger-btn--open");
-		navList.classList.remove("header__nav-list--active");
+		closeMenu();
 
 		event.preventDefault();
 		const elementId = event.target.getAttribute("href");
